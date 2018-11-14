1ccb0488f26a81743abb19a5b93847972044b813
Jolan Taelman <taelman.jolan@hotmail.Com>
2018-11-14 08:57:09 +0100

    Add project files.

---
+++ ProjectCityApp/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectCityApp.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
