51d0874a0b582e50d17b92d23f0b764040b0b76a
Jolan <taelman.jolan@hotmail.Com>
2018-12-17 21:27:41 +0100

    View implement

--- MvvmLight1/Views/BusinessDetails.xaml.cs
+++ MvvmLight1/Views/BusinessDetails.xaml.cs
@@ -24,6 +24,16 @@ namespace MvvmLight1.Views
 
         public BusinessDetails()
         {
+            var openingsuren = new List<Uren>();
+            openingsuren.Add(new Uren { Dag = "Maandag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Dinsdag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Woensdag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Donderdag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Vrijdag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Zaterdag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+            openingsuren.Add(new Uren { Dag = "Zondag", Openingsuren = DateTime.Now, Sluitingsuren = DateTime.Now });
+
+            this.gekozenBusiness = new Business { Name = "Business 1", Category = "Restaurant", OpeningsUren =  openingsuren};
             this.InitializeComponent();
         }
     }
